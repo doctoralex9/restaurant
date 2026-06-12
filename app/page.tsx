db6cfa20ed431964fe896110d9e7e397d3e18aee
Melas <doctoralex9@gmail.com>
2026-06-12 22:49:36 +0300

    change strategy

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import HeroSection from './sections/HeroSection'
-import VideoScrollSection from './sections/VideoScrollSection'
-import MenuCTASection from './sections/MenuCTASection'
-import FooterSection from './sections/FooterSection'
-
 export default function Page() {
   return (
-    <main className="bg-background overflow-x-hidden">
-      <HeroSection />
-      <VideoScrollSection />
-      <MenuCTASection />
-      <FooterSection />
-    </main>
+    <main className="min-h-screen bg-white" />
   )
 }
